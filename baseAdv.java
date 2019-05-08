aa96c8442f4eb8827cfb47d10833212df68097b1
Samantha Castro <stc84@scarletmail.rutgers.edu>
2019-05-07 23:37:16 -0400

    Create baseAdv.java

---
+++ baseAdv.java
@@ -0,0 +1,26 @@
+package finalproj;
+
+public class baseAdv {
+	//clues
+	
+	boolean clueOne = false; //Hatchet
+	boolean clueTwo = false; //Bloody Glove
+	boolean	clueThree = false; //Bruno Magli Shoe
+	boolean clueFour = false; //Football
+	boolean clueFive = false; //Car Keys
+	boolean clueSix = false; //Underwear
+	boolean clueSeven = false; //Microphone
+
+}
+
+public void rooms() {
+	room[0] = "Ballroom";
+	room[1] = "Dining Room";
+	room[2] = "Conservatory";
+	room[3] = "Kitchen";
+	room[4] = "Lounge";
+	room[5] = "Billiard Room";
+	room[6] = "Library";
+	room[7] = "Study";
+			
+}

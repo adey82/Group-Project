bc101158ab404370e058b5fe0ceb4f01336b15d2
Samantha Castro <stc84@scarletmail.rutgers.edu>
2019-05-07 23:35:53 -0400

    Update textAdv.java

--- textAdv.java
+++ textAdv.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 package finalproj;
 
 public class textAdv {
@@ -5,6 +6,16 @@ public class textAdv {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Welcome to Celebrity Mansion! Today, we have a murder mystery in our hands. We need you to help us figure out whodunnit. What's your name detective?" );
+		System.out.print("Please enter your name:");
+		String detectiveName = input.nextLine();
+		
+		User detective = new User();
+		detective.setName(detectiveName);
+		
+		System.out.println("___________________________________________________________________________________________________");
+
+		System.out.println();
 	}
 
 }
